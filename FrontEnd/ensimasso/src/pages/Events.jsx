5d41373c27a905f68c68153c9429bdcd1b9f2669
thatwasyahya <yahyacoubi64@gmail.com>
2024-11-13 13:52:01 +0100

    retry

--- FrontEnd/ensimasso/src/pages/Events.jsx
+++ FrontEnd/ensimasso/src/pages/Events.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { Timeline } from "../components/Event/Timeline.tsx";
-import './style/Events.css';
 
 function Events() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50 py-16 px-4">
-      <Timeline />
+    <div>
     </div>
   );
 }
