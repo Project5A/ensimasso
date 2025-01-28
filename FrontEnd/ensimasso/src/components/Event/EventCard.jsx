e7e3300cc3b3487841952b2928356d157640c88a
thatwasyahya <yahyacoubi64@gmail.com>
2025-01-28 15:30:09 +0100

    upgrade ui

--- FrontEnd/ensimasso/src/components/Event/EventCard.jsx
+++ FrontEnd/ensimasso/src/components/Event/EventCard.jsx
@@ -41,16 +41,31 @@ const EventCard = ({ event }) => {
             className="event-card-button primary-button"
             aria-label="Mark as going"
           >
-            <FontAwesomeIcon icon={faCheckCircle} className="icon" />
+            <FontAwesomeIcon icon={faCheckCircle} className="icon" style={{ color: 'black' }} />
             Going
           </button>
-          <button
-            className="event-card-button secondary-button"
-            aria-label="Share event"
-          >
-            <FontAwesomeIcon icon={faShareAlt} className="icon" />
-            Share
-          </button>
+          <div className="share">
+            <button>
+              <div className="svg-wrapper-1">
+                <div className="svg-wrapper">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path
+                      fill="currentColor"
+                      d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+              <span>Send</span>
+            </button>
+          </div>
+
           <button
             className="event-card-button toggle-button"
             onClick={toggleDescription}
@@ -59,6 +74,7 @@ const EventCard = ({ event }) => {
             <FontAwesomeIcon
               icon={isExpanded ? faChevronUp : faChevronDown}
               className="icon"
+              style={{ color: 'black' }}
             />
             {isExpanded ? "See Less" : "See More"}
           </button>
