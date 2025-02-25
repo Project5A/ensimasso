25a3f3dc0ccad6ab02cc41cef67fb8df04447369
thatwasyahya <yahyacoubi64@gmail.com>
2025-02-25 21:31:54 +0100

    still working on payement

---
+++ FrontEnd/ensimasso/src/components/Payment/stripePromise.jsx
@@ -0,0 +1,3 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+export const stripePromise = loadStripe('pk_test_51QwTLNHaRUkxlox55AjGwR5M0mtUE8f3vMLMhqKFNrjkGE5NJLBzrI3p9c5Q0JNr0mgueZQrSo1gsSkD6pUn9Lit00ATgDV0ji');
